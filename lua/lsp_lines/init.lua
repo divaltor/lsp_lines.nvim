88a2fa836013bfade7f884ac10d031ad80cc378d
Vlad Vladov <divaltor@pm.me>
2025-01-28 09:58:28 +0200

    Fix autocmd

--- lua/lsp_lines/init.lua
+++ lua/lsp_lines/init.lua
@@ -44,7 +44,7 @@ M.setup = function(options)
 
       vim.api.nvim_clear_autocmds({ group = ns.user_data.virt_lines_augroup, buffer = bufnr })
       if opts.virtual_lines.only_current_line then
-        vim.api.nvim_create_autocmd("CursorMoved", {
+        vim.api.nvim_create_autocmd({ "CursorHold", "DiagnosticChanged" }, {
           buffer = bufnr,
           callback = function()
             render_current_line(diagnostics, ns.user_data.virt_lines_ns, bufnr, opts)
